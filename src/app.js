38067347d1752dd876c88859b124660dc1d6f82f
hcam05 <hcam05@gmail.com>
2018-01-15 18:18:44 -0800

    added dependencies for async await

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PlayerTable from './components/Player/PlayerTable';
+import 'babel-polyfill';
 
 ReactDOM.render(<PlayerTable />, document.getElementById('mount'));
