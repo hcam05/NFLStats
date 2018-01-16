6d2528789b7330652556a41e9f8f71a57571dcd3
hcam05 <hcam05@gmail.com>
2018-01-15 18:19:43 -0800

    working on modularizing api fetch function

--- src/model/playerData.js
+++ src/model/playerData.js
@@ -1,9 +1,9 @@
 'use strict';
 const isofetch = require('isomorphic-fetch');
 
-const playerData = (season, wkNum) => {
+const playerData = (year, week) => {
   const allPlayers = []
-  const nflApiUrlSeason = `http://api.fantasy.nfl.com/v1/players/stats?statType=weekStats&season=${this.state.year}&week=${this.state.week}&format=json`;
+  const nflApiUrlSeason = `http://api.fantasy.nfl.com/v1/players/stats?statType=weekStats&season=${year}&week=${week}&format=json`;
   console.log(`getting data ${nflApiUrlSeason}`);
   fetch(nflApiUrlSeason)
     .then((resp) => resp.json())
@@ -16,15 +16,13 @@ const playerData = (season, wkNum) => {
         plyr.team = x.teamAbbr;
         plyr.seasonPts = x.seasonPts;
         plyr.weekPts = x.weekPts;
-        plyr.season = this.state.season;
-        plyr.week = this.state.week;
-        plyr.yrWkId = this.state.season + '' + this.state.week + '' + x.id;
+        plyr.season = year;
+        plyr.week = week;
+        plyr.yrWkId = year + '' + week + '' + x.id;
         allPlayers.push(plyr);
       })
-      console.log(`in setstate`);
-      this.setState({
-        players: allPlayers
-      });
+      console.log(`length: ${allPlayers.length}`)
+      return allPlayers;
     })
 }
 
