129cf8cb16e05d9efa6e18b138bfab4c62c387bb
hcam05 <hcam05@gmail.com>
2018-01-19 18:21:10 -0800

    commented out defensive players

--- src/components/Controls/PlayerControl.js
+++ src/components/Controls/PlayerControl.js
@@ -11,9 +11,9 @@ const PlayerControl = (props) => {
       <button onClick={() => props.filterTable('TE')}>TE</button>
       <button onClick={() => props.filterTable('DEF')}>DEF</button>
       <button onClick={() => props.filterTable('K')}>K</button>
-      <button onClick={() => props.filterTable('LB')}>LB</button>
+      {/* <button onClick={() => props.filterTable('LB')}>LB</button>
       <button onClick={() => props.filterTable('DB')}>DB</button>
-      <button onClick={() => props.filterTable('DL')}>DL</button>
+      <button onClick={() => props.filterTable('DL')}>DL</button> */}
     </div>
   )
 
