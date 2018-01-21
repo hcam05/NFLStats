4eb4b3ccfc3002aa936f62ca968071ef3751c9d3
hcam05 <hcam05@gmail.com>
2018-01-21 15:33:47 -0800

    working on filter functionality

--- src/components/Player/PlayerStats.js
+++ src/components/Player/PlayerStats.js
@@ -2,26 +2,51 @@ import React from 'react';
 
 class PlayerStats extends React.Component {
 
+  filteredPositions() {
+    let positionArr = Object.keys(this.props.positions);
+    let filtered = Object.values(this.props.positions);
+    const filteredPositions = positionArr.filter(function (x, i) {
+      if (filtered[i]) {
+        return positionArr[i]
+      }
+    })
+    console.log(`it fired: ${filteredPositions}`);
+  };
+
+  componentDidMount() {
+    this.filteredPositions(this.props.positons);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return (nextProps.positions !== this.props.positions);
+  }
+
+  componentDidUpdate() {
+    this.filteredPositions();
+  }
+
   render() {
+
     const playerStats = this.props.data
     const playerList = []
     if (this.props.data.length !== 0) {
       for (let i = this.props.start; i <= this.props.end; i++) {
         // if (this.props.data[i].position === 'RB') {
-          playerList.push(
-            <tr key={this.props.data[i].id}>
-              <td key={`${this.props.data[i].id}name`}>{this.props.data[i].name}</td>
-              <td key={`${this.props.data[i].id}position`}>{this.props.data[i].position}</td>
-              <td key={`${this.props.data[i].id}team`}>{this.props.data[i].team}</td>
-              <td key={`${this.props.data[i].id}seasonPts`}>{this.props.data[i].seasonPts}</td>
-              <td key={`${this.props.data[i].id}weekPts`}>{this.props.data[i].weekPts}</td>
-              <td key={`${this.props.data[i].id}week`}>{this.props.data[i].week}</td>
-              <td key={`${this.props.data[i].id}season`}>{this.props.data[i].season}</td>
-            </tr>
-          )
+        playerList.push(
+          <tr key={this.props.data[i].id}>
+            <td key={`${this.props.data[i].id}name`}>{this.props.data[i].name}</td>
+            <td key={`${this.props.data[i].id}position`}>{this.props.data[i].position}</td>
+            <td key={`${this.props.data[i].id}team`}>{this.props.data[i].team}</td>
+            <td key={`${this.props.data[i].id}seasonPts`}>{this.props.data[i].seasonPts}</td>
+            <td key={`${this.props.data[i].id}weekPts`}>{this.props.data[i].weekPts}</td>
+            <td key={`${this.props.data[i].id}week`}>{this.props.data[i].week}</td>
+            <td key={`${this.props.data[i].id}season`}>{this.props.data[i].season}</td>
+          </tr>
+        )
         // }
       }
     }
+
     return (
       <table key='playertable'>
         <thead>
