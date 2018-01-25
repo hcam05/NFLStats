dbf9113056c9c5090fd362070fd976e8b79ca55e
hcam05 <hcam05@gmail.com>
2018-01-24 22:41:58 -0800

    filter function by position working

--- src/components/Player/PlayerStats.js
+++ src/components/Player/PlayerStats.js
@@ -10,39 +10,62 @@ class PlayerStats extends React.Component {
         return positionArr[i]
       }
     })
-    console.log(`it fired: ${filteredPositions}`);
+    // console.log(`it fired: ${filteredPositions}`);
     this.filteredPlayers(filteredPositions);
   };
-  
-  filteredPlayers(filteredPositions){
-    const filteredPlayers = this.props.data.filter( function(x,i, filteredPositions) {
-      console.log(x.position);
+
+  filteredPlayers(filteredPositions) {
+    const filteredPlayers = this.props.data.filter(function (x, i, filteredPositions) {
+      // console.log(x.position);
       //how to filter all positions in an array
       (x.position !== filteredPositions[i])
     });
-    console.log(filteredPlayers);
+    // console.log(filteredPlayers);
     return filteredPlayers
   }
-  
+
   componentDidMount() {
-    // this.filteredPositions(this.props.positons);
+    // this.createPlayers();
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    return (nextProps.positions !== this.props.positions || nextProps.data != this.props.data);
+    return (nextProps.positions !== this.props.positions || nextProps.data != this.props.data || nextProps.end !== this.props.end);
   }
 
   componentDidUpdate() {
     this.filteredPositions();
   }
 
+  createPlayers() {
+    // const playerStats = this.props.data
+    // const playerList = []
+    // if (this.props.data.length !== 0) {
+    //   for (let i = this.props.start; i <= this.props.end; i++) {
+    //     // if (this.props.data[i].position === 'RB') {
+    //     playerList.push(
+    //       <tr key={this.props.data[i].id}>
+    //         <td key={`${this.props.data[i].id}name`}>{this.props.data[i].name}</td>
+    //         <td key={`${this.props.data[i].id}position`}>{this.props.data[i].position}</td>
+    //         <td key={`${this.props.data[i].id}team`}>{this.props.data[i].team}</td>
+    //         <td key={`${this.props.data[i].id}seasonPts`}>{this.props.data[i].seasonPts}</td>
+    //         <td key={`${this.props.data[i].id}weekPts`}>{this.props.data[i].weekPts}</td>
+    //         <td key={`${this.props.data[i].id}week`}>{this.props.data[i].week}</td>
+    //         <td key={`${this.props.data[i].id}season`}>{this.props.data[i].season}</td>
+    //       </tr>
+    //     )
+    // }
+    //   }
+    // }
+    // return playerList;
+  }
+
   render() {
 
     const playerStats = this.props.data
     const playerList = []
-    if (this.props.data.length !== 0) {
+    if (this.props.data.length !== 0 && this.props.showPosition === 'all') {
       for (let i = this.props.start; i <= this.props.end; i++) {
-        // if (this.props.data[i].position === 'RB') {
+        // if (this.props.data[i].position === this.props.showPosition) {
         playerList.push(
           <tr key={this.props.data[i].id}>
             <td key={`${this.props.data[i].id}name`}>{this.props.data[i].name}</td>
@@ -55,6 +78,22 @@ class PlayerStats extends React.Component {
           </tr>
         )
         // }
+      }
+    } else {
+      for (let i = this.props.start; i <= this.props.end; i++) {
+        if (this.props.data[i].position === this.props.showPosition) {
+          playerList.push(
+            <tr key={this.props.data[i].id}>
+              <td key={`${this.props.data[i].id}name`}>{this.props.data[i].name}</td>
+              <td key={`${this.props.data[i].id}position`}>{this.props.data[i].position}</td>
+              <td key={`${this.props.data[i].id}team`}>{this.props.data[i].team}</td>
+              <td key={`${this.props.data[i].id}seasonPts`}>{this.props.data[i].seasonPts}</td>
+              <td key={`${this.props.data[i].id}weekPts`}>{this.props.data[i].weekPts}</td>
+              <td key={`${this.props.data[i].id}week`}>{this.props.data[i].week}</td>
+              <td key={`${this.props.data[i].id}season`}>{this.props.data[i].season}</td>
+            </tr>
+          )
+        }
       }
     }
 
