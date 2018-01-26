1102503ee90be5ef6425f8ba48e86883533bded2
hcam05 <hcam05@gmail.com>
2018-01-26 13:35:45 -0800

    minor changes updated id to root

--- src/app.js
+++ src/app.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import PlayerTable from './components/Player/PlayerTable';
 import 'babel-polyfill';
 
-ReactDOM.render(<PlayerTable />, document.getElementById('mount'));
+ReactDOM.render(<PlayerTable />, document.getElementById('root'));
