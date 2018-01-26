59869d1f83aa1d829a4aa44f9c342232a669e48c
hcam05 <hcam05@gmail.com>
2018-01-25 22:51:14 -0800

    added "All Postions" button

--- src/components/Controls/PlayerControl.js
+++ src/components/Controls/PlayerControl.js
@@ -11,6 +11,7 @@ const PlayerControl = (props) => {
       <button onClick={() => props.filterTable('TE')}>TE</button>
       <button onClick={() => props.filterTable('DEF')}>DEF</button>
       <button onClick={() => props.filterTable('K')}>K</button>
+      <button onClick={() => props.filterTable('all')}>All Positions</button>
       {/* <button onClick={() => props.filterTable('LB')}>LB</button>
       <button onClick={() => props.filterTable('DB')}>DB</button>
       <button onClick={() => props.filterTable('DL')}>DL</button> */}
