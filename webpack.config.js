131c78fd30ffe0b1aa00dc36fc1b8fd1782ad96f
hcam05 <hcam05@gmail.com>
2018-01-26 13:33:17 -0800

    added css loaders to webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,37 @@
 const path = require('path');
 
 module.exports = {
- context: path.join(__dirname, 'src'),
- entry: [
-  'babel-polyfill',
-   './app.js',
- ],
- output: {
-   path: path.join(__dirname, 'www'),
-   filename: 'bundle.js',
- },
- module: {
-   rules: [
-     {
-       test: /\.js$/,
-       exclude: /node_modules/,
-       use: [
-         'babel-loader',
-       ],
-     },
-   ],
- },
- resolve: {
-   modules: [
-     path.join(__dirname, 'node_modules'),
-   ],
- },
+  context: path.join(__dirname, 'src'),
+  entry: [
+    'babel-polyfill',
+    './app.js',
+  ],
+  output: {
+    path: path.join(__dirname, 'www'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+    ],
+  },
+  resolve: {
+    modules: [
+      path.join(__dirname, 'node_modules'),
+    ],
+  },
 };
