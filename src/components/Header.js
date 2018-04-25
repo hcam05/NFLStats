83b66b881bf11e317191a38d66e65086b653fe8d
hcam05 <hcam05@gmail.com>
2018-04-24 20:42:07 -0700

    created Header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      2017 Fantasy Football Stats Dashboard
+    </div>
+  )
+}
+
+export default Header;
